39bea0ff966028acc989bd60bedb58d2558c7290
christian figueiredo <developer.figueiredo@gmail.com>
2025-03-20 11:44:50 -0300

    add usercontroller

--- src/Backend/MyRecipeBook.API/Controllers/UserController.cs
+++ src/Backend/MyRecipeBook.API/Controllers/UserController.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MyRecipeBook.Communication.Request;
+using MyRecipeBook.Communication.Response;
 
 namespace MyRecipeBook.API.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     public class UserController : ControllerBase
     {
-        
+        [HttpPost]
+        [ProducesResponseType(typeof(ResponseRegisterUserJson),StatusCodes.Status201Created)]
+        public IActionResult Register(RequestRegisterUserJson request)
+        {
+            return Created();
+        }
     }
 }
